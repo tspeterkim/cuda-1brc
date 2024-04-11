da869b1c98a723b88b4e3f8ccc74036c2c81fbf2
Peter Kim <tspeterkim3@gmail.com>
2024-04-12 00:18:01 +0900

    add cmts

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
+# Create the 1 billion row file as measurements.txt
 python data/create_measurements.py 1000000000
-nvcc -o fast fast.cu -O2
-./fast data/measurements.txt 1000000 600000
+
+# Compile and run the c++ baseline.
+g++ -o base -O2 base.cpp
+time ./base data/measurements.txt  # ~17 mins
+
+# Compile and run my cuda solution.
+nvcc -o fast -O2 fast.cu
+# 1 million threads to execute in batches of max size 600000.
+# TODO: Increase the max size by using a GPU with more memory.
+time ./fast data/measurements.txt 1000000 600000  # ~17s on V100
+
+# Sanity check
+# TODO: The cuda floats are off by 0.1 for some cities. Check why.
+diff cuda_measurements.out measurements.out
